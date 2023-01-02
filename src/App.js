9a561a7a47cfd876c44a8ccb935978ee90951b7f
biruk_anley <birukanley2022@gmail.com>
2023-01-02 22:52:09 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
-const  App=()=> {
+const App = () => {
+  //5.32.15
   return (
     <>
       <Header />
