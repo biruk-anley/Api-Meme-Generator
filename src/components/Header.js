c6dd0a0bc4303bbd80167103789d5a52464203fd
biruk_anley <birukanley2022@gmail.com>
2023-01-02 11:10:04 +0300

    add header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import memeIcon from '../images/meme2.png'
+const Header=()=> {
+  return (
+      <div className='header'>
+          <img src={ memeIcon} className="header--image" />
+          <h2 className='header--title'>Meme Generator</h2>
+          <h2 className='header--project'>react project</h2>
+
+      </div>
+  )
+}
+
+export default Header;
