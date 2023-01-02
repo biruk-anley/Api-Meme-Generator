a45bc583f16047d6586488a50a03cd4ecfefde51
biruk_anley <birukanley2022@gmail.com>
2023-01-02 11:09:57 +0300

    add meme.js

---
+++ src/components/Meme.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const Meme=()=> {
+  return (
+    <main className='Form'>
+      <form className='form'>
+          <input type='text' placeholder='shut up' className='form--input' />
+          <input type='text' placeholder='Take Money' className='form--input' />
+        <button className='form--button'>
+          Get a new meme image  🖼
+          </button>
+      </form>
+         
+          
+      </main>
+  )
+}
+
+export default Meme;
