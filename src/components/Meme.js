80a85296c0111ce1d794b9bdbadba48c04b3985e
biruk_anley <birukanley2022@gmail.com>
2023-01-02 22:52:25 +0300

    update some functions

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,18 @@
 import React from 'react'
-const Meme=()=> {
+import memesData from '../memesData'
+const Meme = () => {
+  const handleOnClick = () => {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+  }
   return (
     <main className='Form'>
       <form className='form'>
           <input type='text' placeholder='shut up' className='form--input' />
           <input type='text' placeholder='Take Money' className='form--input' />
-        <button className='form--button'>
+        <button className='form--button' onClick={handleOnClick}>
           Get a new meme image  🖼
           </button>
       </form>
