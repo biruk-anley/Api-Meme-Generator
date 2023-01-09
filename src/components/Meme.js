dc741c9dbd9ecbbbc1f39b72fdab06173e45ea30
biruk_anley <birukanley2022@gmail.com>
2023-01-09 11:09:39 +0300

    add random images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,29 @@
 import React from 'react'
+
 import memesData from '../memesData'
 const Meme = () => {
+ const [memeImage, setMemeImage] = React.useState("")
+
   const handleOnClick = () => {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-    const url = memesArray[randomNumber].url
-    console.log(url)
+        setMemeImage(memesArray[randomNumber].url)
+        
     
   }
+
   return (
     <main className='Form'>
-      <form className='form'>
+      <div className='form'>
           <input type='text' placeholder='shut up' className='form--input' />
           <input type='text' placeholder='Take Money' className='form--input' />
         <button className='form--button' onClick={handleOnClick}>
           Get a new meme images
             
-          </button>
-      </form>
+        </button>
+       
+      </div>
+       <img src={memeImage} alt="new iamge is added" className='meme__image'/>
          
           
       </main>
