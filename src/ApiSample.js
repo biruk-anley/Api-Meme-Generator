c7b3abf860c60e700dc7cea9c29d8e19d92f7a8f
biruk_anley <birukanley2022@gmail.com>
2023-01-15 10:07:39 +0300

    doc shows how to use an api

---
+++ src/ApiSample.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    
+    //console.log(starWarsData)---this statement makes a component to be renderd many times
+    // and that is why we use side effect called useEffect
+    
+    fetch("https://swapi.dev/api/people/1")
+        .then(res => res.json())
+        .then(data => setStarWarsData(data))
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
